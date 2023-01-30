4890181ba29efb5f77f6fb277688c686190f5f4a
anistone9 <ani.stone.v@gmail.com>
2023-01-29 20:36:08 -0700

    pushed to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,33 +3,30 @@
 // in the html.
 $(function () {
 
+  var timeBlockEl = $('.time-block');
+  var saveButtonEl = $('.saveBtn');
   var timeDisplayEl = $('#currentDay');
-
   //Function to display current date at the top of the page, in the header
   function displayTime() {
     var showToday = dayjs().format('dddd, MMMM D');
     timeDisplayEl.text(showToday);
   }
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  //Function to check the current time via dayjs, then pull the hour ID from the time-block element in the HTML file, 
+  //grab only the number from the ID, and compare it to the current time. The styling for the past, current, and future classes
+  //is applied based on this check.
+  //Had to add "hour" to currentHour check against hourNow for present time, because isSame checks all the way down to miliseconds,
+  //and if the use is not fast enough clicking, the delay then registers and it applies the future class
   function checkHour() {
     var currentHour = dayjs();
 
-    var timeBlockEl = $('.time-block');
-
     for (var i = 0; i < timeBlockEl.length; i++) {
-      var hourId = timeBlockEl[i].id.substring(5, 7);
+      var hourId = timeBlockEl[i].id.substring(5);
       var hourNow = dayjs().hour(hourId);
 
       if (hourNow.isBefore(currentHour)) {
         timeBlockEl[i].classList.add('past');
-      } else if (hourNow.isSame(currentHour)) {
+      } else if (hourNow.isSame(currentHour, "hour")) {
         timeBlockEl[i].classList.add('present');
       } else if (hourNow.isAfter(currentHour)) {
         timeBlockEl[i].classList.add('future');
@@ -37,10 +34,66 @@ $(function () {
     }
   }
 
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  //First read events from local storage and return an array of event objects.
+  //If no events are stored (when the user first opens the application), then create an empty array and return it.
+  function readEventsFromStorage() {
+    var events = localStorage.getItem('events');
+    if (events) {
+      events = JSON.parse(events);
+    } else {
+      events = [];
+    }
+    return events;
+  }
+
+  //Function to grab the array of events from the previous function, and save them in local storage.
+  //Takes the array of events created via the events variable above, and saves it as a string to local memory.
+  function saveEventsToStorage(events) {
+    localStorage.setItem('events', JSON.stringify(events));
+  }
+
+  function handleSaveEvents(event) {
+    event.preventDefault();
+    console.log(event.currentTarget);
+
+    //Create variable for the parend div of button pressed
+    var hourIdEl = $(this).parent('.time-block');
+    var saveId = hourIdEl.attr('id');
+    console.log(hourIdEl);
+
+    //read the user input from textarea
+    var eventEl = hourIdEl.children('.description');
+    var userEvent = eventEl.val();
+
+    //save to local storage the user text for a particular hour
+    if (userEvent == "") {
+      localStorage.removeItem(saveId);
+    } else {
+      localStorage.setItem(saveId, JSON.stringify(userEvent));
+    }
+  }
+
+  //Grab the events from local storage and display them
+  function printEvents() {
+
+    //Get events from storage
+    //var events = readEventsFromStorage();
+
+    for (var i = 0; i < timeBlockEl.length; i++) {
+      var readId = timeBlockEl[i].id;
+      //console.log(readId);
+      var storedValue = localStorage.getItem(readId);
+      //console.log(storedValue);
+      var textAreaEl = timeBlockEl[i].children[1];
+      //console.log(textAreaEl[1].tagName);
+      textAreaEl.value = storedValue;
+    }
+  }
+
+
+
   displayTime();
   checkHour();
+  printEvents();
+  saveButtonEl.on('click', handleSaveEvents);
 });
